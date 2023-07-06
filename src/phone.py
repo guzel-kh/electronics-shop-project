1e83a20df724d08b39db4d5f6a3d746bce4d3929
guzel <guzel.kh23@gmail.ru>
2023-07-06 17:14:12 +0300

    New subclass added, __init__ and __repr__ overridden

---
+++ src/phone.py
@@ -0,0 +1,12 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim) -> None:
+        super().__init__(name, price, quantity)
+        if number_of_sim < 1:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.number_of_sim = number_of_sim
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(\'{self.name}\', {self.price}, {self.quantity}, {self.number_of_sim})"
