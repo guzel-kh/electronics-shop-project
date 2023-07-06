c0948b23e8be697c2e1c1801f69b8807645503af
guzel <guzel.kh23@gmail.ru>
2023-07-06 17:09:15 +0300

    new func added: __add__

--- src/item.py
+++ src/item.py
@@ -27,6 +27,11 @@ class Item:
     def __str__(self) -> str:
         return self.__name
 
+    def __add__(self, other):
+        if issubclass(other.__class__, self.__class__):
+            return self.quantity + other.quantity
+        return None
+
     @property
     def name(self):
         return self.__name
