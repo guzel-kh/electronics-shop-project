e1deae276a5ed23d70fbc549bcd7a4680f3df84b
guzel <guzel.kh23@gmail.ru>
2023-07-09 14:40:52 +0300

    Fixed after checking

--- src/keyboard.py
+++ src/keyboard.py
@@ -5,27 +5,31 @@ class KeyboardMixin:
     """
     Класс-миксин для хранения и изменения раскладки клавиатуры
     """
-    def __init__(self) -> None:
-        self.__language = "EN"
+    SUPPORTED_LANGUAGES = ('EN', 'RU')
 
     @property
-    def language(self) -> None:
+    def language(self) -> str:
         return self.__language
+
+    @language.setter
+    def language(self, value) -> None:
+        if value not in self.SUPPORTED_LANGUAGES:
+            raise ValueError('Unsupported language')
+        self.__language = value
 
     @property
     def change_lang(self):
         """
-        Меняет язык на другой из двух доступных(RU и EN)
+        Меняет язык на следующий из поддерживаемых
         Возвращает self
         """
-        if self.language == "EN":
-            self.__language = "RU"
-        elif self.language == "RU":
-            self.__language = "EN"
+        current_language_index = self.SUPPORTED_LANGUAGES.index(self.__language)
+        next_lang_index = (current_language_index + 1) % len(self.SUPPORTED_LANGUAGES)
+        self.language = self.SUPPORTED_LANGUAGES[next_lang_index]
         return self
 
 
 class Keyboard(Item, KeyboardMixin):
-    def __init__(self, name, price, quantity):
-        super().__init__(name, price, quantity)
-        KeyboardMixin.__init__(self)
+    def __init__(self, name, price, quantity, language='EN'):
+        super().__init__(name=name, price=price, quantity=quantity)
+        self.language = language
